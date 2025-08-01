af040b53752077d8ed038ecf6c2fd3387ad62105
dimitrysheva <dimitry.shev4encko@gmail.com>
2025-08-01 08:14:19 +0300

    Update downtime_app.py

--- downtime_app.py
+++ downtime_app.py
@@ -256,8 +256,17 @@ if df is not None and not df.empty:
             
             if 'Реакція на заявки' not in df_to_convert.columns:
                 df_to_convert['Реакція на заявки'] = ''
+            
+            # Створюємо CSV-файл у пам'яті
+            csv_string_io = StringIO()
+            
+            # Додаємо спеціальний рядок для Excel
+            csv_string_io.write("sep=;\n")
+            
+            # Записуємо DataFrame у CSV
+            df_to_convert.to_csv(csv_string_io, index=False, sep=';', encoding='utf-8-sig')
 
-            return df_to_convert.to_csv(index=False, sep=';', encoding='utf-8-sig')
+            return csv_string_io.getvalue()
 
         with col1:
             st.download_button(
